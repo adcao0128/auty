cd8d52a4057f5fd3ca7709a8c55b0b64256f5798
Ming Bian <mynameisking67@gmail.com>
2024-11-24 13:32:15 -0500

    changed music so that it would give an option for which app to open

--- android/app/src/main/java/com/auty/modules/applets/MusicApplet.java
+++ android/app/src/main/java/com/auty/modules/applets/MusicApplet.java
@@ -1,17 +1,17 @@
 package com.auty.modules.applets;
 
 import android.app.Activity;
-import android.bluetooth.BluetoothDevice;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.os.Build;
+import android.content.pm.ResolveInfo;
 
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MusicApplet extends Applet {
-    
+
     private Context context;
 
     public MusicApplet(Context context) {
@@ -20,12 +20,50 @@ public class MusicApplet extends Applet {
     }
 
     public void launchMusic() {
-        PackageManager packageManager = context.getPackageManager();
-        Intent launchIntent = packageManager.getLaunchIntentForPackage("com.google.android.apps.youtube.music");
 
-        if (launchIntent != null) {
-            launchIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            context.startActivity(launchIntent);
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_APP_MUSIC);
+
+
+        Intent alternateIntent = new Intent(Intent.ACTION_VIEW);
+        alternateIntent.setType("audio/*");
+
+
+        List<ResolveInfo> musicApps = new ArrayList<>();
+
+
+        List<ResolveInfo> mainMusicApps = context.getPackageManager()
+                .queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        musicApps.addAll(mainMusicApps);
+
+
+        List<ResolveInfo> audioApps = context.getPackageManager()
+                .queryIntentActivities(alternateIntent, PackageManager.MATCH_DEFAULT_ONLY);
+
+
+        for (ResolveInfo app : audioApps) {
+            if (!containsPackage(musicApps, app.activityInfo.packageName)) {
+                musicApps.add(app);
+            }
         }
+
+        if (!musicApps.isEmpty()) {
+
+            Intent chooserIntent = Intent.createChooser(intent, "Which app would you like to open?");
+
+
+            chooserIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+            context.startActivity(chooserIntent);
+        }
+    }
+
+    private boolean containsPackage(List<ResolveInfo> list, String packageName) {
+        for (ResolveInfo info : list) {
+            if (info.activityInfo.packageName.equals(packageName)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
